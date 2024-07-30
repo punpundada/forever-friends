6a238e8396e62a1931f2bf1e5b4e690a962d3edc
prajwal <prajwal.parashkar@akronsystems.com>
2024-07-30 18:26:31 +0530

    theme changes

--- app/auth/(authenticated)/layout.tsx
+++ app/auth/(authenticated)/layout.tsx
@@ -1,19 +1,17 @@
-import getUser from '@/actions/userActions';
-import { redirect } from 'next/navigation';
-import React from 'react'
-
+import getUser from "@/actions/userActions";
+import { redirect } from "next/navigation";
+import React from "react";
 
 const layout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-    const user = await getUser()
-    if(user){
-        redirect("/")
-    } 
-  return children
-}
+  const user = await getUser();
+  if (user) {
+    redirect("/");
+  }
+  return children;
+};
 
-
-export default layout
+export default layout;
